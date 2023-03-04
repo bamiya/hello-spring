8c617529af4955e6f77906fdc2dd93c13374aebe
Bamiya <bamiyamy@gmail.com>
2023-03-05 05:48:31 +0900

    Controller,getmapping의 사용법

---
+++ hello-spring/src/main/java/com/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello") // 웹앱에서 hello로 들어오면 여기를 호출해줌
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello"; //리턴값의 hello는 템플릿의 hello.html의 이름인 hello이다.
+    }
+    @GetMapping("bye")
+    public String bye(Model model){
+        model.addAttribute("data", "bye");
+        return "bye";
+    }
+}
