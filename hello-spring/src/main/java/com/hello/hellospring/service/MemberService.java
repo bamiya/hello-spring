0e8c1a84ebb0b63cc5e6d164d759591fd25e95d1
Bamiya <bamiyamy@gmail.com>
2023-03-24 14:24:58 +0900

    JPA 추가와 테스트코드

--- hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
@@ -2,14 +2,12 @@ package com.hello.hellospring.service;
 
 import com.hello.hellospring.domain.Member;
 import com.hello.hellospring.repository.MemberRepository;
-import com.hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
